e2a0f18297d3b8c2e11090789c13a467897a7770
Franciane Aprigio <francianefad@outlook.com>
2024-03-08 01:32:47 -0300

    [FEAT] - Updating Service

--- RabbitMQProductAPI/Service/IProductService.cs
+++ RabbitMQProductAPI/Service/IProductService.cs
@@ -1,6 +1,12 @@
-﻿namespace RabbitMQProductAPI.Service;
+﻿using RabbitMQProductAPI.Models;
+
+namespace RabbitMQProductAPI.Service;
 
 public interface IProductService
 {
-    
+    public IEnumerable<Product> GetProductList();
+    public Product GetProductById(int Id);
+    public Product AddProduct(Product product);
+    public Product UpdateProduct(Product product);
+    public bool DeleteProduct(int Id);
 }
--- RabbitMQProductAPI/Service/ProductService.cs
+++ RabbitMQProductAPI/Service/ProductService.cs
@@ -1,6 +1,45 @@
-﻿namespace RabbitMQProductAPI.Service;
+﻿using RabbitMQProductAPI.Data;
+using RabbitMQProductAPI.Models;
 
-public class ProductService
-{
+namespace RabbitMQProductAPI.Service;
+
+    public class ProductService: IProductService
+    {
+        private readonly DbContextProduct _dbContext;
+        public ProductService(DbContextProduct dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<Product> GetProductList()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product GetProductById(int Id)
+        {
+            return _dbContext.Products.Where(x => x.ProductId == Id).FirstOrDefault();
+        }
+
+        public Product AddProduct(Product product)
+        {
+            var result = _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            var result = _dbContext.Products.Update(product);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+
+        public bool DeleteProduct(int Id)
+        {
+            var filterData = _dbContext.Products.Where(x => x.ProductId == Id).FirstOrDefault();
+            var result = _dbContext.Remove(filterData);
+            _dbContext.SaveChanges();
+            return result != null ? true : false;
+        }
+    }
     
-}
